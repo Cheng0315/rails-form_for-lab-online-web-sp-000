87de22bdd7dc503deab6fe8fa22c0657f30014b1
Cheng Vue <cvu3@ucsc.edu>
2019-01-27 07:00:06 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,15 +11,15 @@ class StudentsController < ApplicationController
     @student.save
     redirect_to student_path(@student)
   end
-   
+
   def update
     @student = Student.find(params[:id])
     @student.update(post_params(:first_name, :last_name))
     redirect_to student_path(@srtudent)
   end
-   
+
   private
-   
+
   def post_params(*args)
     params.require(:student).permit(*args)
   end

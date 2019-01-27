027d5b33f0bd81210356d4ebe7878c7fe2a42d35
Cheng Vue <cvu3@ucsc.edu>
2019-01-27 07:05:22 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -7,8 +7,20 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
+    @school_class = SchoolClass.new(post_params(:title, :room_number))
+    @school_class.save
+    redirect_to student_path(@school_class)
   end
 
   def update
+    @school_class = Student.find(params[:id])
+    @school_class.update(post_params(:title, :room_number))
+    redirect_to student_path(@school_class)
+  end
+
+  private
+
+  def post_params(*args)
+    params.require(:student).permit(*args)
   end
 end
